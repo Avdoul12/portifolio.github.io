681ce08029fbe9243bf9cdc1fd58affa95846d10
Avdoul12 <abdulmalikkabiru1234@gmail.com>
2025-01-06 11:04:59 +0100

    Add files via upload

---
+++ my portifolio/scripts.js
@@ -0,0 +1,8 @@
+
+const btn = document.querySelector(".hamburger");
+const open = document.querySelector(".nav-links");
+
+
+btn.addEventListener("click", function(){
+    open.classList.toggle("show")
+})
